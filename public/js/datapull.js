231c29e8fd6730af06a8212b4771e848d01710c7
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-25 14:47:58 -1000

    startign to get set up/ understand

--- public/js/datapull.js
+++ public/js/datapull.js
@@ -8,19 +8,30 @@ window.onload = function(){
       httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
   }
 
-  //All browsers
-  httpRequest.onreadystatechange = function(){
-      if (httpRequest.readyState === 4) {
-        if (httpRequest.status === 200) {
-          updateUI(httpRequest.responseText);
-        } else {
-          throw new Error('There was a problem with the request.');
-        }
-      }
-  }
-
-
   httpRequest.open('GET', '../api/my_boards.json', true);
   httpRequest.send(null);
 
+  //All browsers
+  httpRequest.onreadystatechange = function(){
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        updateUI(httpRequest.responseText)
+      } else {
+        throw new Error('There was a problem with the request.');
+      }
+    }
+  }
+
+var data;
+
+  function updateUI(theData){
+    data = JSON.parse(theData);
+    console.log(data);
+  }
+
+  console.log('outside', data);
+
+        //updateUI(httpRequest.responseText);
 }
+
+
