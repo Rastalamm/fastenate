dadf822a3afd1a54324fba3fd547d18a91d820cd
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-25 17:00:46 -1000

    starting to implement js and create elements on the page

--- public/js/datapull.js
+++ public/js/datapull.js
@@ -1,5 +1,35 @@
 window.onload = function(){
 
+  var contentHouse = document.getElementsByTagName('content-house')
+
+  var articleHouse = document.createElement('div');
+  articleHouse.setAttribute('class', 'article-house');
+  contentHouse.appendChild(articleHouse);
+
+
+
+  var imageDiv = document.createElement('div');
+  imageDiv.setAttribute('class', 'article-pic');
+  imageDiv.style.backgroundImage = GRABFROMOBJ;
+  imageDiv.style.backgroundSize = 'cover';
+
+  var image
+
+
+
+
+
+
+
+  var HTTP_UNSENT = 0;
+  var HTTP_OPENED = 1;
+  var HTTP_HEADERS_RECV = 2;
+  var HTTP_LOADING = 3;
+  var HTTP_DONE = 4;
+  var HTTP_STATUS_OK = 200;
+  var HTTP_STATUS_NOT_FOUND = 404;
+  var HTTP_STATUS_SERVER_ERROR = 500;
+  var HTTP_GET = 'GET';
   var httpRequest;
 
   if (window.XMLHttpRequest) { // Mozilla, Safari, IE7+ ...
@@ -11,27 +41,36 @@ window.onload = function(){
   httpRequest.open('GET', '../api/my_boards.json', true);
   httpRequest.send(null);
 
-  //All browsers
   httpRequest.onreadystatechange = function(){
-    if (httpRequest.readyState === 4) {
-      if (httpRequest.status === 200) {
-        updateUI(httpRequest.responseText)
-      } else {
-        throw new Error('There was a problem with the request.');
+      if (httpRequest.readyState === HTTP_DONE) {
+        if (httpRequest.status === HTTP_STATUS_OK) {
+          var res = JSON.parse(httpRequest.responseText);
+          updateUI(res);
+          console.log('res', res);
+        } else {
+          throw new Error('There was a problem with the request.');
+        }
       }
+  }
+
+  function updateUI (pretendJson){
+    var imageID;
+    var imagey;
+
+    for(var i = 0; i < 4; i++){
+      imageID = document.getElementById('image' + i);
+      console.log('here', imageID);
+
+      imagey = pretendJson.data.children[i].data.url;
+      imageID.style.backgroundImage = "url('" + imagey + "')";
+
+
+  console.log(pretendJson.data.children[0].data.url);
     }
+
+
+
+
   }
-
-var data;
-
-  function updateUI(theData){
-    data = JSON.parse(theData);
-    console.log(data);
-  }
-
-  console.log('outside', data);
-
-        //updateUI(httpRequest.responseText);
 }
-
 
